d98b7843c4e27b3b4759e737999aaca56af1fce4
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-05-04 11:26:25 +0300

    Initial commit

---
+++ src/main/java/GrayScaleConverter.java
@@ -0,0 +1,31 @@
+import edu.duke.*;
+import java.io.*;
+
+public class GrayScaleConverter {
+	//I started with the image I wanted (inImage)
+	public ImageResource makeGray(ImageResource inImage) {
+		//I made a blank image of the same size
+
+		//for each pixel in outImage
+
+			//look at the corresponding pixel in inImage
+
+			//compute inPixel's red + inPixel's blue + inPixel's green
+			//divide that sum by 3 (call it average)
+
+			//set pixel's red to average
+
+			//set pixel's green to average
+
+			//set pixel's blue to average
+
+
+		//outImage is your answer
+	}
+
+	public void testGray() {
+		ImageResource ir = new ImageResource();
+		ImageResource gray = makeGray(ir);
+		gray.draw();
+	}
+}
